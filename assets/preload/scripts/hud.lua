0737bb266cb0d35adea9a88f42d258bcd48d04ce
gabzsa <109770528+gabzsa@users.noreply.github.com>
2022-09-10 17:10:35 -0300

    Add files via upload

---
+++ assets/preload/scripts/hud.lua
@@ -0,0 +1,9 @@
+function onCreatePost()
+	setTextFont("scoreTxt", "arial.ttf");
+	setProperty("timeBarBG.visible", false);
+    setProperty("timeBar.visible", false);
+    setProperty("timeTxt.visible", false);
+	setTextFont("botplayTxt", "arial.ttf");
+    setTextColor("botplayTxt", "0xFFFFFF");
+    setTextString("botplayTxt", "fucking cheater");
+end
